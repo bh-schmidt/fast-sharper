2ab4817511344b63c2a8a3b2489286d862a04945
Bruno Hammerschmidt <re4deto@hotmail.com>
2020-04-13 22:36:34 -0300

    run sonar file updated.

--- src/RunSonar.sh
+++ src/RunSonar.sh
@@ -1,25 +1,39 @@
 set -e
 cat << eof
 
-Updating Sonnar
++---------------------------+
+| Sonarqube update started  |
++---------------------------+
 
 eof
 
-PROJECT_KEY="FastSharper"
+COLLECT_COVERAGE="true"
+COVERLET_OUTPUT_FORMAT="opencover"
 SONAR_SCANNER_LOCATION="sonarscanner"
-OPENCOVER_LOCATION="FastSharper.Tests\coverage.opencover.xml"
+
+PROJECT_KEY="FastSharper"
 TOKEN="de773e5d0e2808a211aab1c85c6a2327362b3850"
+OPENCOVER_LOCATION="FastSharper.Tests\coverage.opencover.xml"
 COVERAGE_EXCLUSIONS="Tests/**/*.cs"
 
 
-dotnet test //p:CollectCoverage=true //p:CoverletOutputFormat=opencover
+dotnet test \
+    //p:CollectCoverage=$COLLECT_COVERAGE \
+    //p:CoverletOutputFormat=$COVERLET_OUTPUT_FORMAT
 dotnet build-server shutdown
-dotnet sonarscanner begin //k:$PROJECT_KEY //d:sonar.login=$TOKEN //d:sonar.cs.opencover.reportsPaths=$OPENCOVER_LOCATION /d:sonar.coverage.exclusions=$COVERAGE_EXCLUSIONS
+dotnet sonarscanner begin \
+    //k:$PROJECT_KEY \
+    //d:sonar.login=$TOKEN \
+    //d:sonar.cs.opencover.reportsPaths=$OPENCOVER_LOCATION \
+    /d:sonar.coverage.exclusions=$COVERAGE_EXCLUSIONS
 dotnet build
-dotnet sonarscanner end //d:sonar.login=$TOKEN
+dotnet sonarscanner end \
+    //d:sonar.login=$TOKEN
 
 cat << eof
 
-Sonar update succeeded
++----------------------------+
+| Sonarqube update succeeded |
++----------------------------+
 
 eof
